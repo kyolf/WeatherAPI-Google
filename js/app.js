f9d94a541c33405e21261492fadface43e3ac36f
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-09-10 21:41:14 -0700

    got css for tabs working properly

--- js/app.js
+++ js/app.js
@@ -23,8 +23,8 @@ const appState = {
     weather:{},
     temp:null,
     clouds:null,
-    date:null,
-    rh:null
+    rh:null,
+    showing: true
   },
   tmrWeather:{
     city:{
@@ -38,8 +38,8 @@ const appState = {
     weather:{},
     temp:null,
     clouds:null,
-    date:null,
-    rh:null
+    rh:null,
+    showing: false
   },
   dayAfterTmrWeather:{
     city:{
@@ -53,26 +53,18 @@ const appState = {
     weather:{},
     temp:null,
     clouds:null,
-    date:null,
-    rh:null
+    rh:null,
+    showing: false
   }
 };
 
-//rearrange Dates to MM/DD/YYYY
-function reArrangeDates(date){
-  const day = date.substring(8,10);
-  const month = date.substring(5,7);
-  const year = date.substring(0,4);
-  return `${month}/${day}/${year}`;
-}
-
 //Getting the string of the date
-function getDateString(date){
+function getDateString(date) {
   const curDay = date.getDate();
   const curMonth = date.getMonth() + 1;
 
-  const day = (curDay > 10) ? curDay : `0${curDay}`;
-  const month = (curMonth > 10) ? curMonth : `0${curMonth}`;
+  const day = (curDay >= 10) ? curDay : `0${curDay}`;
+  const month = (curMonth >= 10) ? curMonth : `0${curMonth}`;
   const year = date.getFullYear();
   return `${month}/${day}/${year}`;
 }
@@ -113,7 +105,7 @@ function clearMarker(state) {
 }
 
 //Modify the curWeather Values
-function setCurWeatherValuesToState(state, data){
+function setCurWeatherValuesToState(state, data) {
   const curWeather = state.curWeather;
   
   if(data){
@@ -124,7 +116,6 @@ function setCurWeatherValuesToState(state, data){
     curWeather.weather = data.weather;
     curWeather.temp = data.temp;
     curWeather.clouds = data.clouds;
-    curWeather.date = data.datetime;
     curWeather.rh = data.rh;
   }
   
@@ -132,7 +123,7 @@ function setCurWeatherValuesToState(state, data){
 }
 
 //Modify the Forecast Values
-function setForecastWeatherValuesToState(forecast, response, data){
+function setForecastWeatherValuesToState(forecast, response, data) {
   if(response){
     forecast.city.city_name = response.city_name;
     forecast.city.country = response.country_code;
@@ -141,7 +132,6 @@ function setForecastWeatherValuesToState(forecast, response, data){
     forecast.weather = data.weather;
     forecast.temp = data.temp;
     forecast.clouds = data.clouds;
-    forecast.date = data.datetime;
     forecast.rh = data.rh;
   }
   
@@ -149,7 +139,7 @@ function setForecastWeatherValuesToState(forecast, response, data){
 }
 
 //render the weather values
-function renderWeather(stateDate, element){
+function renderWeather(stateDate, element) {
   element.html(`
     <p class="city_name">${stateDate.city.city_name}, ${stateDate.city.country}</p>
     <img class="weather_icons" src="icons/${stateDate.weather.icon}.png"/>
@@ -164,29 +154,68 @@ function renderWeather(stateDate, element){
 }
 
 //render the dates
-function renderDates(element){
+function renderDates(element) {
   const today = new Date();
   const tmr = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
   const dayAfterTmr = new Date(new Date().getTime() + 48 * 60 * 60 * 1000);
   element.html(`
-    <article class="date_box">
+    <article class="date_box today_js">
       <p>${getDateString(today)}</p>
     </article>
-    <article class="date_box">
+    <article class="date_box gray date_border tmr_js">
       <p>${getDateString(tmr)}</p>
     </article>
-    <article class="date_box">
+    <article class="date_box gray date_border dayAfterTmr_js">
       <p>${getDateString(dayAfterTmr)}</p>
     </article>`);
     
 }
 
-function eventListeners(state){
-  renderWeather(state.curWeather, $('.weather_info'));
+//change correct Tabs
+function tabChange(curWeatherState, curElement, changedWeatherState, changedElement) {
+  if(curWeatherState.showing){
+    curWeatherState.showing = false;
+    changedWeatherState.showing = true;
+    changeDateTabsCss(curElement, changedElement);
+  }
+}
+
+//Changing the colors and borders of the tabs
+function changeDateTabsCss(curElement, changedElement) {
+  curElement.addClass('gray');
+  curElement.addClass('date_border');
+
+  changedElement.removeClass('gray');
+  changedElement.removeClass('date_border');
+}
+
+function eventListeners(state) {
+  const curWeather = state.curWeather;
+  const tmrWeather = state.tmrWeather;
+  const dayAfter = state.dayAfterTmrWeather;
+
+  $('.date').on('click', '.today_js', (event) => {
+    renderWeather(state.curWeather, $('.weather_info'));
+    tabChange(tmrWeather, $('.tmr_js'), curWeather, $('.today_js'));
+    tabChange(dayAfter, $('.dayAfterTmr_js'), curWeather, $('.today_js'));    
+  });
+
+  $('.date').on('click', '.tmr_js', (event) => {
+    renderWeather(state.tmrWeather, $('.weather_info'));
+    tabChange(curWeather, $('.today_js'), tmrWeather, $('.tmr_js'));
+    tabChange(dayAfter, $('.dayAfterTmr_js'), tmrWeather, $('.tmr_js'));
+  });
+
+  $('.date').on('click', '.dayAfterTmr_js', (event) => {
+    renderWeather(state.dayAfterTmrWeather, $('.weather_info'));
+    tabChange(curWeather, $('.today_js'), dayAfter, $('.dayAfterTmr_js'));
+    tabChange(tmrWeather, $('.tmr_js'), dayAfter, $('.dayAfterTmr_js'));
+  });
+
 }
 
 //WeatherBit API
-function fetchWeatherBitApi(state){
+function fetchWeatherBitApi(state) {
   const query = {
     key: 'e5763a0d45a14f7385be5287fa59bdc0',
     units: 'I',
@@ -200,8 +229,11 @@ function fetchWeatherBitApi(state){
     setCurWeatherValuesToState(appState, response.data[0]);
     renderWeather(state.curWeather, $('.weather_info'));
   });
+
   $.getJSON('https://api.weatherbit.io/v2.0/forecast/3hourly', query, function(response){
     console.log('hi',response);
+    setForecastWeatherValuesToState(state.tmrWeather, response, response.data[0]);
+    setForecastWeatherValuesToState(state.dayAfterTmrWeather, response, response.data[8]);
   });
 }
 
@@ -232,7 +264,7 @@ function initMap() {
 
   //clicking on google maps
   google.maps.event.addDomListener(map, 'click', callbackGoogle);
-  // eventListeners(appState);
+  eventListeners(appState);
 
 }
 
@@ -250,7 +282,7 @@ function getYourCoords(infoWindow, state) {
 
       callbackGoogle(null);
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found. Click Anywhere On the Map or Enter ZipCode to Begin!');
+      infoWindow.setContent('Location found.\n Click Anywhere On the Map or Enter ZipCode to Begin!');
       infoWindow.open(state.map);
       state.map.setCenter(pos);
     }, function() {
@@ -267,7 +299,7 @@ function getYourCoords(infoWindow, state) {
 function handleLocationError(browserHasGeolocation, infoWindow, map) {
   infoWindow.setPosition(map.getCenter());
   infoWindow.setContent(browserHasGeolocation ?
-    'The Geolocation service was blocked. Click Anywhere On the Map or Enter ZipCode to Begin!' :
-    'Your browser doesn\'t support geolocation. Click Anywhere On the Map or Enter ZipCode to Begin!');
+    'The Geolocation service was blocked.\n Click Anywhere On the Map or Enter ZipCode to Begin!' :
+    'Your browser doesn\'t support geolocation.\n Click Anywhere On the Map or Enter ZipCode to Begin!');
   infoWindow.open(map);
 }
