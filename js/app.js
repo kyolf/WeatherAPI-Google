d7d849a1e219c9b663d7c3c7d8ac98f6f69d3a9d
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-09-10 18:14:39 -0700

    push icons from weatherbit got the curWeather stuff showing and looking good in mobile

--- js/app.js
+++ js/app.js
@@ -16,11 +16,66 @@ const appState = {
       city_name:null,
       country:null
     },
-    windspd:{
-
-    }
+    wind:{
+      windDir:null,
+      windSpd:null,
+    },
+    weather:{},
+    temp:null,
+    clouds:null,
+    date:null,
+    rh:null
+  },
+  tmrWeather:{
+    city:{
+      city_name:null,
+      country:null
+    },
+    wind:{
+      windDir:null,
+      windSpd:null,
+    },
+    weather:{},
+    temp:null,
+    clouds:null,
+    date:null,
+    rh:null
+  },
+  dayAfterTmrWeather:{
+    city:{
+      city_name:null,
+      country:null
+    },
+    wind:{
+      windDir:null,
+      windSpd:null,
+    },
+    weather:{},
+    temp:null,
+    clouds:null,
+    date:null,
+    rh:null
   }
 };
+
+//rearrange Dates to MM/DD/YYYY
+function reArrangeDates(date){
+  const day = date.substring(8,10);
+  const month = date.substring(5,7);
+  const year = date.substring(0,4);
+  return `${month}/${day}/${year}`;
+}
+
+//Getting the string of the date
+function getDateString(date){
+  const curDay = date.getDate();
+  const curMonth = date.getMonth() + 1;
+
+  const day = (curDay > 10) ? curDay : `0${curDay}`;
+  const month = (curMonth > 10) ? curMonth : `0${curMonth}`;
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+}
 
 //Set map to google maps
 function setMap(map, state) {
@@ -37,7 +92,7 @@ function setLatLng(pos, state) {
 }
 
 //Set Marker Lat and Lng
-function setMarkerLatLng(data, state) {
+function setMarkerLatLng(state, data) {
   const markerLoc = state.markerLocation;
   markerLoc.lat = data.latLng.lat();
   markerLoc.lng = data.latLng.lng();
@@ -57,24 +112,93 @@ function clearMarker(state) {
   state.marker.map(el => el.setMap(null));
 }
 
-//Getting the string of the date
-function getDateString(date){
-  const day = (date.getDate() > 10) ? date.getDate() : `0${date.getDate()}`;
-  const month = (date.getMonth()+1 > 10) ? date.getMonth()+1 : `0${date.getMonth()+1}`;
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
+//Modify the curWeather Values
+function setCurWeatherValuesToState(state, data){
+  const curWeather = state.curWeather;
+  
+  if(data){
+    curWeather.city.city_name = data.city_name;
+    curWeather.city.country = data.country_code;
+    curWeather.wind.windDir = data.wind_cdir_full;
+    curWeather.wind.windSpd = data.wind_spd;
+    curWeather.weather = data.weather;
+    curWeather.temp = data.temp;
+    curWeather.clouds = data.clouds;
+    curWeather.date = data.datetime;
+    curWeather.rh = data.rh;
+  }
+  
+  return curWeather;
+}
+
+//Modify the Forecast Values
+function setForecastWeatherValuesToState(forecast, response, data){
+  if(response){
+    forecast.city.city_name = response.city_name;
+    forecast.city.country = response.country_code;
+    forecast.wind.windDir = data.wind_cdir_full;
+    forecast.wind.windSpd = data.wind_spd;
+    forecast.weather = data.weather;
+    forecast.temp = data.temp;
+    forecast.clouds = data.clouds;
+    forecast.date = data.datetime;
+    forecast.rh = data.rh;
+  }
+  
+  return forecast;  
+}
+
+//render the weather values
+function renderWeather(stateDate, element){
+  element.html(`
+    <p class="city_name">${stateDate.city.city_name}, ${stateDate.city.country}</p>
+    <img class="weather_icons" src="icons/${stateDate.weather.icon}.png"/>
+    <p class="weather_description">${stateDate.weather.description}</p>
+    <div class="weather_details">
+      <p>${stateDate.temp}&deg;F</p>
+      <p>${stateDate.rh}% humidity</p>
+      <p>${stateDate.clouds}% cloudy</p>
+    </div>
+    <p class="wind_details">Wind is blowing at ${stateDate.wind.windSpd} mph
+       in the ${stateDate.wind.windDir} direction.</p>`);
+}
+
+//render the dates
+function renderDates(element){
+  const today = new Date();
+  const tmr = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  const dayAfterTmr = new Date(new Date().getTime() + 48 * 60 * 60 * 1000);
+  element.html(`
+    <article class="date_box">
+      <p>${getDateString(today)}</p>
+    </article>
+    <article class="date_box">
+      <p>${getDateString(tmr)}</p>
+    </article>
+    <article class="date_box">
+      <p>${getDateString(dayAfterTmr)}</p>
+    </article>`);
+    
+}
+
+function eventListeners(state){
+  renderWeather(state.curWeather, $('.weather_info'));
 }
 
 //WeatherBit API
 function fetchWeatherBitApi(state){
   const query = {
     key: 'e5763a0d45a14f7385be5287fa59bdc0',
+    units: 'I',
     lat: state.markerLocation.lat || state.yourLoc.lat,
     lon: state.markerLocation.lng || state.yourLoc.lng,
   };
 
   $.getJSON('https://api.weatherbit.io/v2.0/current', query, (response) => {
     console.log(response);
+    renderDates($('.date'));
+    setCurWeatherValuesToState(appState, response.data[0]);
+    renderWeather(state.curWeather, $('.weather_info'));
   });
   $.getJSON('https://api.weatherbit.io/v2.0/forecast/3hourly', query, function(response){
     console.log('hi',response);
@@ -85,7 +209,7 @@ function fetchWeatherBitApi(state){
 function callbackGoogle(response) {
   if (response !== null) { 
     clearMarker(appState);
-    setMarkerLatLng(response, appState);
+    setMarkerLatLng(appState, response);
     makeMarker(appState);
   }
   fetchWeatherBitApi(appState);
@@ -126,7 +250,7 @@ function getYourCoords(infoWindow, state) {
 
       callbackGoogle(null);
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent('Location found. Click Anywhere On the Map or Enter ZipCode to Begin!');
       infoWindow.open(state.map);
       state.map.setCenter(pos);
     }, function() {
@@ -143,7 +267,7 @@ function getYourCoords(infoWindow, state) {
 function handleLocationError(browserHasGeolocation, infoWindow, map) {
   infoWindow.setPosition(map.getCenter());
   infoWindow.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
+    'The Geolocation service was blocked. Click Anywhere On the Map or Enter ZipCode to Begin!' :
+    'Your browser doesn\'t support geolocation. Click Anywhere On the Map or Enter ZipCode to Begin!');
   infoWindow.open(map);
 }
